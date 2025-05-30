ebc741ba1dca3ab7d0ec6628e0692e7d03f15049
smileface <funtt8@gmail.com>
2025-05-30 20:10:45 +0200

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,8 +47,21 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let result;
+
+  for (const key in books) {
+    if (books[key].title === title) {
+      result = books[key];
+      break;
+    }
+  }
+
+  if (!result) {
+    return res.status(404).json({ message: `Book with title ${title} not found`});
+  }
+
+  return res.status(200).json(result);
 });
 
 //  Get book review
