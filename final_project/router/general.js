c70ef7784258eb323aa6b6949a12f6c885bdebcb
smileface <funtt8@gmail.com>
2025-05-30 19:46:45 +0200

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,13 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  if (!isbn || !books.hasOwnProperty(isbn)) {
+    return res.status(404).json({ message: `Book with ISBN ${isbn} not found`});
+  }
+
+  return res.status(200).json(books[isbn]);
  });
   
 // Get book details based on author
